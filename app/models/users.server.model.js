a3ef21e6110eef9c4190992411e88a225c3bd4a9
jhar <jhar87@gmail.com>
2015-09-16 01:13:16 -0700

    feat: Adds UsersSchema pre-save method, hashPassword(), and authenticate()

--- app/models/users.server.model.js
+++ app/models/users.server.model.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose'),
+	crypto = require('crypto'),
 	Schema = mongoose.Schema;
 
 var UsersSchema = new Schema({
@@ -14,7 +15,35 @@ var UsersSchema = new Schema({
 	joined: {
 		type: Date,
 		default: Date.now
+	},
+	salt: String,
+	provider: {
+		type: String,
+		required: true
+	},
+	providerId: String,
+	providerData: {}
+});
+
+UsersSchema.pre('save', function(next) {
+	if (this.password) {
+		this.salt = new Buffer(crypto.randomBytes(16).toString('base64'), 'base64');
+		this.password = this.hashPassword(this.password);
 	}
+	next();
+});
+
+UsersSchema.methods.hashPassword = function(password) {
+	return crypto.pbkdf2Sync(password, this.salt, 10000, 64).toString('base64');
+};
+
+UsersSchema.methods.authenticate = function(password) {
+	return this.password === this.hashPassword(password);
+}
+
+UsersSchema.set('toJSON', {
+	getters: true,
+	virtuals: true
 });
 
 mongoose.model('Users', UsersSchema);
