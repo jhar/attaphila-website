71c9b9a8fa65de3d309dd2cef716a6a5694368e2
jhar <jhar87@gmail.com>
2015-09-20 14:34:19 -0700

    Configured Hashbangs

--- public/application.js
+++ public/application.js
@@ -2,6 +2,12 @@ var mainAppModuleName = 'attaphila';
 
 var mainAppModule = angular.module(mainAppModuleName, ['ngRoute', 'example']);
 
+mainAppModule.config(['$locationProvider',
+	function($locationProvider) {
+		$locationProvider.hashPrefix('!');
+	}
+]);
+
 angular.element(document).ready(function() {
 	angular.bootstrap(document, [mainAppModuleName]);
 });
