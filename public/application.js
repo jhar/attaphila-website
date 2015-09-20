c11f929688a91b8f01734adfdc89846f02600465
jhar <jhar87@gmail.com>
2015-09-20 02:34:18 -0700

    Reverts binding example, sets up example of Angular module

--- public/application.js
+++ public/application.js
@@ -1,6 +1,6 @@
 var mainAppModuleName = 'attaphila';
 
-var mainAppModule = angular.module(mainAppModuleName, []);
+var mainAppModule = angular.module(mainAppModuleName, ['example']);
 
 angular.element(document).ready(function() {
 	angular.bootstrap(document, [mainAppModuleName]);
