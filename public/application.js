4c6ed16ad7e888b25859d3055548088ae9ee6f35
jhar <jhar87@gmail.com>
2015-09-20 14:32:05 -0700

    Added ngRoute module

--- public/application.js
+++ public/application.js
@@ -1,6 +1,6 @@
 var mainAppModuleName = 'attaphila';
 
-var mainAppModule = angular.module(mainAppModuleName, ['example']);
+var mainAppModule = angular.module(mainAppModuleName, ['ngRoute', 'example']);
 
 angular.element(document).ready(function() {
 	angular.bootstrap(document, [mainAppModuleName]);
