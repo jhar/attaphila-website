5a398db0eeee21a8a1c6c3d38d7f6ab1b4c53520
jhar <jhar87@gmail.com>
2015-09-26 15:13:38 -0700

    Finishes adding the user authentication service

--- public/application.js
+++ public/application.js
@@ -1,12 +1,14 @@
 var mainAppModuleName = 'attaphila';
 
-var mainAppModule = angular.module(mainAppModuleName, ['ngRoute', 'example']);
+var mainAppModule = angular.module(mainAppModuleName, ['ngRoute', 'users', 'example']);
 
 mainAppModule.config(['$locationProvider',
 	function($locationProvider) {
 		$locationProvider.hashPrefix('!');
 	}
 ]);
+
+if (window.location.hash === '#_=_') window.location.hash = '#!';
 
 angular.element(document).ready(function() {
 	angular.bootstrap(document, [mainAppModuleName]);
