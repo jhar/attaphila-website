8008454e44a0b423c135898c86ad421646d95f3c
jhar <jhar87@gmail.com>
2015-09-29 14:22:59 -0700

    Finalizes CRUD module implementation

--- public/example/controllers/example.client.controller.js
+++ public/example/controllers/example.client.controller.js
@@ -1,5 +1,5 @@
 angular.module('example').controller('ExampleController', ['$scope', 'Authentication',
 	function($scope, Authentication) {
-		$scope.name = Authentication.user ? Authentication.user.username : 'Attaphila Application';
+		$scope.authentication = Authentication;
 	}
 ]);
