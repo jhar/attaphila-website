c649652af99359972d18ee15b0be42df29d8e562
Justin Harrison <jhar87@gmail.com>
2015-10-30 01:52:55 +0000

    CSS/UI tweaks + signin flash message fix

--- public/application.js
+++ public/application.js
@@ -1,6 +1,6 @@
 var mainAppModuleName = 'attaphila';
 
-var mainAppModule = angular.module(mainAppModuleName, ['ngResource', 'ngRoute', 'angularFileUpload', 'users', 'example', 'posts']);
+var mainAppModule = angular.module(mainAppModuleName, ['ngResource', 'ngRoute', 'angularFileUpload', 'users', 'posts']);
 
 mainAppModule.config(['$locationProvider',
 	function($locationProvider) {
@@ -20,25 +20,3 @@ if (window.location.hash === '#_=_') window.location.hash = '#!';
 angular.element(document).ready(function() {
 	angular.bootstrap(document, [mainAppModuleName]);
 });
-
-/* Stick title bar to top - not necessary if no hero element
-
-$(window).scroll(function () {
-	var scrollTop = $(window).scrollTop();
-    var top = $('.navbar').offset().top;
-    var bottom = 450;
-
-    if (scrollTop > top) {
-        //when the header reaches the top of the window change position to fixed
-        $('.navbar').css('position', 'fixed');
-        $('.navbar').css('top', 0);
-    }
-
-    if (scrollTop < bottom) {
-    	$('.navbar').css('position', 'absolute');
-    	$('.navbar').css('top', 450);
-    }
-
-});
-
-*/
