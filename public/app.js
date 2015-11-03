ea40f63623b87ee39a6f131c25b7ff2efa8ee1a3
Justin Harrison <jhar87@gmail.com>
2015-11-03 23:34:18 +0000

    Single page view works, FormattedDate component added

--- public/app.js
+++ public/app.js
@@ -112,7 +112,7 @@ var PostPreview = React.createClass({
 		return (
 			<article className="row">
 				<div className="col-xs-12">
-					<a href={"/posts/" + this.props.post.category + "/" + this.props.post._id}>
+					<Link to={"/posts/" + this.props.post.category + "/" + this.props.post._id}>
 						<div className="media">
 							<div className="media-left media-middle">
 								<img className="media-object" src={this.props.post.coverPhotoURL} />
@@ -123,9 +123,124 @@ var PostPreview = React.createClass({
 								<span>Click to read more.</span>
 							</div>
 						</div>
-					</a>
+					</Link>
 				</div>
 			</article>
+		);
+	}	
+});
+
+var SinglePost = React.createClass({
+	loadPostFromServer: function(cat, pid) {
+		var url = "/api/posts/" + cat + "/" + pid;
+		$.ajax({
+			url: url,
+			dataType: 'json',
+			cache: false,
+			success: function(data) {
+				this.setState({
+					post: data
+				});
+			}.bind(this),
+			error: function(xhr, status, err) {
+				console.error(url, status, err.toString());
+			}.bind(this)
+		});	
+	},
+	componentDidMount: function() {
+		this.loadPostFromServer(this.props.params.category, this.props.params.postid);		
+	},
+	getInitialState: function() {
+		return {
+			post: {
+				creator: {},
+				medialinks: [{}]
+			}
+		};
+	},
+	render: function() {
+		console.log(this.state.post.creator.username);
+		return (
+			<section className="page">
+				<div className="row">
+					<div className="col-xs-0 col-sm-1 col-md-2 col-lg-2"></div>
+					<div className="col-xs-12 col-sm-10 col-md-8 col-lg-8">
+						<img className="img-responsive cover-photo" src={this.state.post.coverPhotoURL} />
+						<h2>{this.state.post.title}</h2>
+					</div>
+					<div className="col-xs-0 col-sm-1 col-md-2 col-lg-2"></div>
+				</div>
+				<div className="row">
+					<div className="col-xs-0 col-sm-1 col-md-2 col-lg-2"></div>
+					<div className="col-xs-12 col-sm-10 col-md-8 col-lg-8">
+						<p>{this.state.post.content}</p>
+						<small>
+							<em>Posted on </em>
+							<em>
+								<FormattedDate date={this.state.post.created} />
+							</em>
+							<em> by </em>
+							<em>{this.state.post.creator.username}</em>
+						</small>
+						<div>
+							// TODO: Only show if user._id == post.creator._id
+							<a href="/posts/{this.state.post.category}/{this.state.post._id}/edit">edit</a>
+							// TODO: Make this delete the post
+							<a href="#">delete</a>
+						</div>
+					</div>
+					<div className="col-xs-0 col-sm-1 col-md-2 col-lg-2"></div>
+				</div>
+				<div className="row">
+					<div className="col-xs-0 col-sm-1 col-md-2 col-lg-2"></div>
+					<div className="col-xs-12 col-sm-10 col-md-8 col-lg-8">
+						// TODO: media in post.medialinks
+						<div>
+							<div>
+								<h3>
+									// TODO: if media.media == 'article', then
+							  			a href=media.url Go to Page /a
+							  	</h3>
+							</div>
+							<div className="embed-responsive embed-responsive-16by9">
+							  <iframe className="embed-responsive-item">
+							  	TODO: if media.media == 'youtube', src=media.url
+							  </iframe>
+							</div>
+							<div className="img-responsive">
+								TODO: only display img src=media.url if media.media == 'photo'
+							</div>
+						</div>
+					</div>
+					<div className="col-xs-0 col-sm-1 col-md-2 col-lg-2"></div>
+				</div>
+			</section>	
+		);
+	}	
+});
+
+var FormattedDate = React.createClass({
+	render: function() {
+		var months = [
+				'January',
+				'February',
+				'March',
+				'April',
+				'May',
+				'June',
+				'July',
+				'August',
+				'September',
+				'October',
+				'November',
+				'December'
+		];
+		var date = new Date(this.props.date);
+		var day = date.getDate();
+		var monthIndex = date.getMonth();
+		var year = date.getFullYear();
+		return (
+			<span>{months[monthIndex] + ' ' + day + ', ' + year}</span>	
 		);
 	}	
 });
@@ -133,7 +248,8 @@ var PostPreview = React.createClass({
 render(
 	<Router>
 		<Route path="/" component={App}>
-			<Route path="/posts/:category" component={PostsByCategory} />
+			<Route path="posts/:category" component={PostsByCategory} />
+			<Route path="posts/:category/:postid" component={SinglePost} />
 		</Route>
 	</Router>,
 	document.getElementById('app')
