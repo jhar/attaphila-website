4490c66300062779a2ca5b0821496ffd052db59a
Justin Harrison <jhar87@gmail.com>
2015-12-12 02:02:14 +0000

    Concats CSS & JS

--- gulpfile.js
+++ gulpfile.js
@@ -6,14 +6,15 @@ var babelify = require('babelify');
 var source = require('vinyl-source-stream');
 var less = require('gulp-less');
 var util = require('gulp-util');
+var concat = require('gulp-concat');
 
 /* Compile Less Files */
 
 gulp.task('compile-less', function() {
-    gulp.src('./public/src/css/main.less')
+    return gulp.src('./public/src/css/main.less')
         .pipe(less())
         .on('error', util.log)
-        .pipe(gulp.dest('./public/dist/css/'))
+        .pipe(gulp.dest('./public/src/css/'))
         .on('error', util.log);
 });
 
@@ -24,7 +25,24 @@ gulp.task('transpile-js', function() {
         .transform('babelify', { presets: ['es2015', 'react']})
         .bundle()
         .pipe(source('bundle.js'))
+        .pipe(gulp.dest('./public/src/'));
+});
+
+/* Package JavaScript */
+
+gulp.task('scripts', function() {
+    return gulp.src(['./public/src/lib/jquery/dist/jquery.min.js', './public/src/lib/bootstrap/js/modal.js', './public/src/bundle.js'])
+        .pipe(concat('all.js'))
         .pipe(gulp.dest('./public/dist/'));
 });
 
-gulp.task('default', ['compile-less', 'transpile-js']);
+/* Package CSS */
+
+gulp.task('styles', function() {
+    return gulp.src(['./public/src/lib/bootstrap/dist/css/bootstrap.min.css', './public/src/css/main.css'])
+        .pipe(concat('all.css'))
+        .pipe(gulp.dest('./public/dist'));
+});
+
+gulp.task('compile', ['compile-less', 'transpile-js']);
+gulp.task('package', ['scripts', 'styles']);
