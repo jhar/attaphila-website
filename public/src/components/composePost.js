4c696f57471bf7823dcfda65662676730d6e5878
Justin Harrison <jhar87@gmail.com>
2015-12-15 08:26:43 +0000

    Adds new media links

--- public/src/components/composePost.js
+++ public/src/components/composePost.js
@@ -15,7 +15,7 @@ export class ComposePost extends React.Component {
 				}
 			}
 		} else {
-			console.error("Error in CreateOrEditPost constructor: mode or post not passed as prop");
+			console.error("Error in ComposePost constructor: mode or post not passed as prop");
 		}
 		this.handleChange = this.handleChange.bind(this);
 	}
@@ -40,7 +40,7 @@ export class ComposePost extends React.Component {
 				}.bind(this)
 			});
 		} else {
-			console.error("Error in CreateOrEditPost sendPostRequest(): wrong mode");
+			console.error("Error in ComposePost sendPostRequest(): wrong mode");
 		}
 	}
 	sendPutRequest() {
@@ -61,57 +61,57 @@ export class ComposePost extends React.Component {
 		}
 	}
 	render() {
-		var coeHeader, coeMethod, coeTitle, coeCat, coeCover, coeContent, coeMedia;
+		var comHeader, comMethod, comTitle, comCat, comCover, comContent, comMedia;
 		if (this.props.mode == 'create') {
-			coeHeader = "Create New Post";
-			coeMethod = "post";
-			coeTitle = <input type="text" name="title" placeholder="Title" required />;
-			coeCat = <select name="category" required>
+			comHeader = "Create New Post";
+			comMethod = "post";
+			comTitle = <input type="text" name="title" placeholder="Title" required />;
+			comCat = <select name="category" required>
 					 	<option value="inside">Inside</option>
 						<option value="outside">Outside</option>
 						<option value="relatives">Relatives</option>
 						<option value="anthro">Anthropodicies</option>
 					 </select>;
-			coeCover = <input type="text" name="coverPhotoURL" placeholder="Cover Photo URL" />;
-			coeContent = <textarea cols="30" rows="10" name="content" placeholder="Content" required></textarea>;
+			comCover = <input type="text" name="coverPhotoURL" placeholder="Cover Photo URL" />;
+			comContent = <textarea cols="30" rows="10" name="content" placeholder="Content" required></textarea>;
 		} else {
-			coeHeader = "Edit Existing Post";
-			coeMethod = "";
-			coeTitle = <input type="text" name="title" value={this.state.post.title} onChange={this.handleChange} required />;
-			coeCat = <select name="category" value={this.state.post.category} onChange={this.handleChange} required>
+			comHeader = "Edit Existing Post";
+			comMethod = "";
+			comTitle = <input type="text" name="title" value={this.state.post.title} onChange={this.handleChange} required />;
+			comCat = <select name="category" value={this.state.post.category} onChange={this.handleChange} required>
 					 	<option value="inside">Inside</option>
 						<option value="outside">Outside</option>
 						<option value="relatives">Relatives</option>
 						<option value="anthro">Anthropodicies</option>
 					</select>;
-			coeCover = <input type="text" name="coverPhotoURL" value={this.state.post.coverPhotoURL} onChange={this.handleChange} />;
-			coeContent = <textarea cols="30" rows="10" name="content" value={this.state.post.content} onChange={this.handleChange} required></textarea>;
+			comCover = <input type="text" name="coverPhotoURL" value={this.state.post.coverPhotoURL} onChange={this.handleChange} />;
+			comContent = <textarea cols="30" rows="10" name="content" value={this.state.post.content} onChange={this.handleChange} required></textarea>;
 		}
 		return (
 			<section className="page">
 				<div className="col-xs-12">
-					<h1>{coeHeader}</h1>
-					<form action="/api/posts" method={coeMethod}>
+					<h1>{comHeader}</h1>
+					<form action="/api/posts" method={comMethod}>
 						<div>
 							<label>Title</label>
 							<div>
-								{coeTitle}
+								{comTitle}
 							</div>
 						</div>
 						<div>
 							<label>Category</label>
 							<br />
-							{coeCat}
+							{comCat}
 						</div>
 						<div>
 			                <label>Cover Photo URL</label>
 			                <br />
-			                {coeCover}
+			                {comCover}
 			            </div>
 						<div>
 							<label>Content</label>
 							<div>
-								{coeContent}
+								{comContent}
 							</div>
 						</div>
 						<ComposeMediaLinks mode={this.props.mode} post={this.state.post} handleChange={this.handleChange.bind(this)}/>
@@ -132,9 +132,23 @@ export class ComposePost extends React.Component {
 class ComposeMediaLinks extends React.Component {
 	constructor(props) {
 		super(props);
+		this.state = {
+			post: this.props.post
+		};
+		this.handleAdd = this.handleAdd.bind(this);
 	}
-	handleAdd(event) {
-		console.info(this);
+	handleAdd() {
+		console.log(this.state.post);
+		var postCopy = this.state.post;
+		if (this.state.post.medialinks) {
+			var mediaLinksCopy = this.state.post.medialinks;
+			postCopy['medialinks'] = mediaLinksCopy.concat({});
+		} else {
+			postCopy['medialinks'] = [{}];
+		}
+		this.setState({
+			post: postCopy	
+		});
 	}
 	render() {
 		var mediaLinkNodes = this.state.post.medialinks.map(function (mediaLink) {
