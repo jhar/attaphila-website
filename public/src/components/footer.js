e666326c32a9e83c36a6edbfa51898caf9cd5e6f
Justin Harrison <jhar87@gmail.com>
2015-12-20 09:43:14 +0000

    Fixes problem with modal loading

--- public/src/components/footer.js
+++ public/src/components/footer.js
@@ -4,7 +4,7 @@ export class Footer extends React.Component {
 	render() {
 
 		if (this.props.user == 'null') {
-			return <SignIn />;
+			return <SignInButton />;
 		} else {
 			return <SignOut username={this.props.user.username} changeView={this.props.changeView.bind(this)} />;
 		}
@@ -12,23 +12,11 @@ export class Footer extends React.Component {
 	}	
 }
 
-class SignIn extends React.Component {
+class SignInButton extends React.Component {
 	render() {
 		return (
 			<div className="col-xs-12 text-center">
 				<button type="button" className="btn btn-primary" data-toggle="modal" data-target="#signInModal">Admin</button>
-				<div className="modal fade" id="signInModal" tabIndex="-1" role="dialog" aria-labelledby="myModalLabel">
-				  <div className="modal-dialog" role="document">
-				    <div className="modal-content">
-						<form action="/signin" className="form-sign-in" method="post">
-							<img className="img-responsive signinImage" src="/img/scrapbook.png" />
-							<input className="form-control" type="text" name="username" placeholder="Username" required autofocus />
-							<input className="form-control" type="password" name="password" placeholder="Password" required />
-							<button className="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-						</form>
-				    </div>
-				  </div>
-				</div>
 			</div>
 		);
 	}	
